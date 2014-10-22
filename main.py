9e354a17a1a51cb5e945bce2dc9b47a5627b02cf
Camilo Ramirez <camilortte@hotmail.com>
2014-10-21 19:47:01 -0500

    Se agrego la función para el TimeOut

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
+from __future__ import with_statement
 import sched, time
 from testInternet import main
+import signal, time
+from contextlib import contextmanager
 
-TIEMPO=60*20
+class TimeoutException(Exception): pass
+ 
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException, "Timed out!"
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+ 
+ 
+TIEMPO=60*10  #El test se realiza cada 10 minutos
+TIME_OUT=60*5 #DEspues de 5 se termina.
 s = sched.scheduler(time.time, time.sleep)
 def print_time():
-    s.enter(TIEMPO, 1, print_time, ())
-    try:
-        main()
-    except Exception, e:
-        raise e
+	try:
+	    with time_limit(TIME_OUT):
+		    s.enter(TIEMPO, 1, print_time, ())
+		    try:
+		        main()
+		    except Exception, e:
+		        raise e
+	except TimeoutException, msg:
+	    print msg
+	
 
 s.enter(0, 1, print_time, ())
 s.run()
+
+
